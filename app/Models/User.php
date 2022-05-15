2755a355a6fc152d39b1816b58eaa2380a785f17
talers69 <talers69@163.com>
2022-05-15 15:13:03 +0800

    用户信息

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,8 @@ class User extends Authenticatable implements MustVerifyEmail, JWTSubject
     protected $hidden = [
         'password',
         'remember_token',
+        'weixin_openid',
+        'weixin_unionid'
     ];
 
     /**
