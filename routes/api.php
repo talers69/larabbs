b99d6e44cd88cc5f311021e7a8f2a7896c05c6b8
talers69 <talers69@163.com>
2022-05-15 20:21:05 +0800

    发布话题

--- routes/api.php
+++ routes/api.php
@@ -46,6 +46,8 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
         Route::get('users/{user}', 'UsersController@show')->name('users.show');
         // 分类列表
         Route::get('categories', 'CategoriesController@index')->name('categories.index');
+        // 话题列表，详情
+        Route::resource('topics', 'TopicsController')->only(['index', 'show']);
 
         // 登录后可以访问的接口
         Route::middleware('auth:api')->group(function() {
@@ -55,6 +57,8 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
             Route::patch('user', 'UsersController@update')->name('user.update');
             // 上传图片
             Route::post('images', 'ImagesController@store')->name('images.store');
+            // 发布话题
+            Route::resource('topics', 'TopicsController')->only(['store', 'update', 'destroy']);
         });
     });
 });
