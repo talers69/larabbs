f8777ac642b1c5c3f053219cde8fb236811bc20f
talers69 <talers69@163.com>
2022-05-16 21:44:32 +0800

    安装 Passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,97 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
-use Illuminate\Support\Facades\Auth;
+use Auth;
 use Spatie\Permission\Traits\HasRoles;
-use Illuminate\Support\Str;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements MustVerifyEmail, JWTSubject
+class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use HasApiTokens, HasFactory, Notifiable, MustVerifyEmailTrait, HasRoles;
-    use Traits\ActiveUserHelper, Traits\LastActivedAtHelper;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'phone',
-        'email',
-        'password',
-        'introduction',
-        'avatar',
-        'weixin_openid',
-        'weixin_unionid'
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-        'weixin_openid',
-        'weixin_unionid'
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    public function setPasswordAttribute($value)
-    {
-        // 如果值的长度等于 60，即认为是已经做过加密的情况
-        if (strlen($value) != 60) {
-
-            // 不等于 60，做密码加密处理
-            $value = bcrypt($value);
-        }
-
-        $this->attributes['password'] = $value;
-    }
-
-    public function setAvatarAttribute($path)
-    {
-        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
-        if ( ! Str::startsWith($path, 'http')) {
-
-            // 拼接完整的 URL
-            $path = config('app.url') . "/uploads/images/avatars/$path";
-        }
-
-        $this->attributes['avatar'] = $path;
-    }
-
-    public function isAuthorOf($model)
-    {
-        return $this->id == $model->user_id;
-    }
-
-    public function topics()
-    {
-        return $this->hasMany(Topic::class);
-    }
-
-    public function replies()
-    {
-        return $this->hasMany(Reply::class);
-    }
+    use Traits\ActiveUserHelper;
+    use Traits\LastActivedAtHelper;
+    use HasRoles;
+    use HasFactory, MustVerifyEmailTrait;
 
     use Notifiable {
         notify as protected laravelNotify;
@@ -112,6 +35,44 @@ class User extends Authenticatable implements MustVerifyEmail, JWTSubject
         $this->laravelNotify($instance);
     }
 
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'password',
+        'introduction',
+        'avatar',
+        'weixin_openid',
+        'weixin_unionid',
+        'registration_id'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'weixin_openid',
+        'weixin_unionid'
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
     public function markAsRead()
     {
         $this->notification_count = 0;
@@ -119,14 +80,27 @@ class User extends Authenticatable implements MustVerifyEmail, JWTSubject
         $this->unreadNotifications->markAsRead();
     }
 
-    public function getJWTIdentifier()
+    public function setPasswordAttribute($value)
     {
-        return $this->getKey();
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
     }
 
-    public function getJWTCustomClaims()
+    public function setAvatarAttribute($path)
     {
-        return [];
-    }
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if ( ! \Str::startsWith($path, 'http')) {
 
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
+    }
 }
