c41a7d3192f8edfbc78555f516b047e42de20c83
talers69 <talers69@163.com>
2022-05-16 17:53:51 +0800

    活跃用户

--- routes/api.php
+++ routes/api.php
@@ -56,6 +56,8 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
         Route::get('users/{user}/replies', 'RepliesController@userIndex')->name('users.replies.index');
         // 资源推荐
         Route::get('links', 'LinksController@index')->name('links.index');
+        // 活跃用户
+        Route::get('actived/users', 'UsersController@activedIndex')->name('actived.users.index');
 
         // 登录后可以访问的接口
         Route::middleware('auth:api')->group(function() {
